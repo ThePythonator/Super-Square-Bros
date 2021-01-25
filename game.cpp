5464b378c2ee0db1702b973dd2ef45814e13b354
ThePythonator <49040244+ThePythonator@users.noreply.github.com>
2021-01-25 15:55:28 +0000

    Initial commit.

--- game.cpp
+++ game.cpp
@@ -1,6 +1,193 @@
 #include "game.hpp"
+#include "assets.hpp"
+
+#define SCREEN_WIDTH 160
+#define SCREEN_HEIGHT 120
 
 using namespace blit;
+
+enum GameState {
+    MENU_STATE,
+    LEVEL_SELECT_STATE,
+    IN_GAME_STATE,
+    LOST_STATE
+};
+GameState gameState = MENU_STATE;
+
+struct ButtonStates {
+    uint8_t UP;
+    uint8_t DOWN;
+    uint8_t LEFT;
+    uint8_t RIGHT;
+    uint8_t A;
+    uint8_t B;
+    uint8_t X;
+    uint8_t Y;
+    uint8_t JOYSTICK;
+};
+ButtonStates buttonStates = { 0 };
+
+struct SaveData {
+    uint32_t highscore;
+} saveData;
+
+struct Camera {
+    float x, y;
+} camera;
+
+class LevelObject {
+public:
+    LevelObject() {
+        x = y = 0;
+    }
+
+    virtual void update(double dt, ButtonStates buttonStates) = 0;
+
+    virtual void render(Camera camera) = 0;
+
+protected:
+    int x, y;
+};
+
+class Tile : LevelObject {
+public:
+    Tile(int xPosition, int yPosition, uint8_t tileID) {
+        x = xPosition;
+        y = yPosition;
+
+        id = tileID;
+    }
+
+    void render(Camera camera)
+    {
+        screen.sprite(id, Point(camera.x - x, camera.y - y));
+    }
+
+protected:
+    uint8_t id;
+};
+
+class Pickup : LevelObject {
+public:
+    void render(Camera camera) {
+
+    }
+
+protected:
+
+};
+
+class Coin : Pickup {
+public:
+    void render(Camera camera) {
+
+    }
+
+protected:
+
+};
+
+class Entity : LevelObject {
+public:
+    Entity(int xPosition, int yPosition) {
+        x = xPosition;
+        y = yPosition;
+
+        state = IDLE;
+    }
+
+protected:
+    enum EntityState {
+        IDLE,
+        WALK,
+        RUN,
+        JUMP,
+        CROUCH,
+        INJURED,
+        DEAD
+    } state;
+};
+
+class Player : Entity {
+public:
+    void update(double dt, ButtonStates buttonStates) {
+        if (buttonStates.A && state != JUMP) {
+            // Jump.
+            state = JUMP;
+        }
+    }
+
+    void render(Camera camera) {
+
+    }
+
+protected:
+
+};
+
+class Enemy : Entity {
+public:
+    void render(Camera camera) {
+
+    }
+
+protected:
+
+};
+
+//maybe inherit from entity to create specific enemy types, but all should fit into a vector<Enemy> - same interface for each
+
+struct LevelObjects {
+    std::vector<Tile> foreground;
+    std::vector<Tile> background;
+
+    std::vector<Coin> coins;
+
+    std::vector<Enemy> enemies;
+
+    Player player;
+} levelObjects;
+
+void load_level() {
+    levelObjects.foreground.clear();
+    levelObjects.background.clear();
+    levelObjects.coins.clear();
+    levelObjects.enemies.clear();
+    // for tileid in tile ids
+    // levelObjects.foreground(i) = tile
+    // wehen loading tiles, take one off id to get index
+}
+
+void render_tiles(std::vector<Tile> tiles) {
+    for (int i = 0; i < tiles.size(); i++) {
+        tiles.at(i).render(camera);
+    }
+}
+
+void render_coins(std::vector<Coin> coins) {
+    for (int i = 0; i < coins.size(); i++) {
+        coins.at(i).render(camera);
+    }
+}
+
+void render_enemies(std::vector<Enemy> enemies) {
+    for (int i = 0; i < enemies.size(); i++) {
+        enemies.at(i).render(camera);
+    }
+}
+
+void render_level() {
+    render_tiles(levelObjects.background);
+    render_tiles(levelObjects.foreground);
+
+    render_coins(levelObjects.coins);
+}
+
+void render_entities() {
+    render_enemies(levelObjects.enemies);
+
+    levelObjects.player.render(camera);
+}
 
 ///////////////////////////////////////////////////////////////////////////
 //
@@ -9,7 +196,9 @@ using namespace blit;
 // setup your game here
 //
 void init() {
-    set_screen_mode(ScreenMode::hires);
+    set_screen_mode(ScreenMode::lores);
+
+    screen.sprites = Surface::load(asset_sprites);
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -28,9 +217,21 @@ void render(uint32_t time) {
     screen.alpha = 255;
     screen.mask = nullptr;
     screen.pen = Pen(255, 255, 255);
-    screen.rectangle(Rect(0, 0, 320, 14));
+
+    if (gameState == MENU_STATE) {
+        screen.text("Press A to Start", minimal_font, Point(SCREEN_WIDTH / 2, 4), true, TextAlign::center_center);
+    }
+    else if (gameState == LEVEL_SELECT_STATE) {
+
+    }
+    else if (gameState == IN_GAME_STATE) {
+
+    }
+    else if (gameState == LOST_STATE) {
+
+    }
+
     screen.pen = Pen(0, 0, 0);
-    screen.text("Hello 32blit!", minimal_font, Point(5, 4));
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -41,4 +242,17 @@ void render(uint32_t time) {
 // amount if milliseconds elapsed since the start of your game
 //
 void update(uint32_t time) {
+    if (buttons.pressed & Button::A) {
+        if (buttonStates.A == 2) {
+            buttonStates.A = 1;
+        }
+        else {
+            buttonStates.A = 2;
+        }
+    }
+    else {
+        buttonStates.A = 0;
+    }
+
+    printf("%d", asset_assets_level01_tmx[0]);
 }
