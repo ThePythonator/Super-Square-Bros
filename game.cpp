ba51122547a95b83530c45aed60661a38fcb7490
ThePythonator <49040244+ThePythonator@users.noreply.github.com>
2021-01-27 10:13:45 +0000

    Basic tile rendering works!

--- game.cpp
+++ game.cpp
@@ -4,7 +4,31 @@
 #define SCREEN_WIDTH 160
 #define SCREEN_HEIGHT 120
 
+#define EMPTY_TILE_ID 255
+
+#define CAMERA_SCALE 0.1
+
+#define SPRITE_SIZE 8
+
 using namespace blit;
+
+
+const uint8_t SCREEN_MID_WIDTH = SCREEN_WIDTH / 2;
+const uint8_t SCREEN_MID_HEIGHT = SCREEN_HEIGHT / 2;
+
+
+// struct to handle level data header...
+// this will probably need to be revisited/reworked if 32blit-tools has *any* kind of update to it...
+#pragma pack(push,1)
+struct TMX {
+    char head[4];
+    uint8_t empty_tile;
+    uint16_t width;
+    uint16_t height;
+    uint16_t layers;
+    uint8_t data[];
+};
+#pragma pack(pop)
 
 enum GameState {
     MENU_STATE,
@@ -33,12 +57,21 @@ struct SaveData {
 
 struct Camera {
     float x, y;
-} camera;
+
+    Camera() {
+        x = y = 0;
+    }
+};
 
 class LevelObject {
 public:
     LevelObject() {
         x = y = 0;
+    }
+
+    LevelObject(int xPosition, int yPosition) {
+        x = xPosition;
+        y = yPosition;
     }
 
     virtual void update(double dt, ButtonStates buttonStates) = 0;
@@ -51,16 +84,21 @@ protected:
 
 class Tile : LevelObject {
 public:
-    Tile(int xPosition, int yPosition, uint8_t tileID) {
-        x = xPosition;
-        y = yPosition;
+    Tile() : LevelObject() {
+        id = EMPTY_TILE_ID;
+    }
 
+    Tile(int xPosition, int yPosition, uint8_t tileID) : LevelObject(xPosition, yPosition) {
         id = tileID;
+    }
+
+    void update(double dt, ButtonStates buttonStates) {
+
     }
 
     void render(Camera camera)
     {
-        screen.sprite(id, Point(camera.x - x, camera.y - y));
+        screen.sprite(id, Point(SCREEN_MID_WIDTH + x - camera.x, SCREEN_MID_HEIGHT + y - camera.y));
     }
 
 protected:
@@ -69,16 +107,58 @@ protected:
 
 class Pickup : LevelObject {
 public:
-    void render(Camera camera) {
+    Pickup() : LevelObject() {
 
     }
+
+    Pickup(int xPosition, int yPosition) : LevelObject(xPosition, yPosition) {
+
+    }
+
+    virtual void update(double dt, ButtonStates buttonStates) = 0;
+
+    virtual void render(Camera camera) = 0;
 
 protected:
 
 };
 
-class Coin : Pickup {
+class AnimatedPickup : Pickup {
 public:
+    AnimatedPickup() : Pickup() {
+
+    }
+
+    AnimatedPickup(int xPosition, int yPosition, std::vector<uint8_t> frames) : Pickup(xPosition, yPosition) {
+
+    }
+
+    void update(double dt, ButtonStates buttonStates) {
+
+    }
+
+    void render(Camera camera) {
+
+    }
+
+protected:
+    std::vector<uint8_t> frames;
+};
+
+class Coin : AnimatedPickup {
+public:
+    Coin() : AnimatedPickup() {
+
+    }
+
+    Coin(int xPosition, int yPosition, std::vector<uint8_t> frames) : AnimatedPickup(xPosition, yPosition, frames) {
+
+    }
+
+    void update(double dt, ButtonStates buttonStates) {
+
+    }
+
     void render(Camera camera) {
 
     }
@@ -89,11 +169,23 @@ protected:
 
 class Entity : LevelObject {
 public:
-    Entity(int xPosition, int yPosition) {
+    Entity() : LevelObject() {
+
+    }
+
+    Entity(int xPosition, int yPosition) : LevelObject(xPosition, yPosition) {
         x = xPosition;
         y = yPosition;
 
         state = IDLE;
+    }
+
+    void update(double dt, ButtonStates buttonStates) {
+
+    }
+
+    void render(Camera camera) {
+
     }
 
 protected:
@@ -110,6 +202,14 @@ protected:
 
 class Player : Entity {
 public:
+    Player() : Entity() {
+
+    }
+
+    Player(int xPosition, int yPosition) : Entity(xPosition, yPosition) {
+
+    }
+
     void update(double dt, ButtonStates buttonStates) {
         if (buttonStates.A && state != JUMP) {
             // Jump.
@@ -121,12 +221,29 @@ public:
 
     }
 
+    void update_camera(Camera camera) {
+        camera.x += (x - camera.x) * CAMERA_SCALE;
+        camera.y += (y - camera.x) * CAMERA_SCALE;
+    }
+
 protected:
 
 };
 
 class Enemy : Entity {
 public:
+    Enemy() : Entity() {
+
+    }
+
+    Enemy(int xPosition, int yPosition) : Entity(xPosition, yPosition) {
+
+    }
+
+    void update(double dt, ButtonStates buttonStates) {
+
+    }
+
     void render(Camera camera) {
 
     }
@@ -145,34 +262,35 @@ struct LevelObjects {
 
     std::vector<Enemy> enemies;
 
+    Camera camera;
+
     Player player;
 } levelObjects;
 
-void load_level() {
-    levelObjects.foreground.clear();
-    levelObjects.background.clear();
-    levelObjects.coins.clear();
-    levelObjects.enemies.clear();
-    // for tileid in tile ids
-    // levelObjects.foreground(i) = tile
-    // wehen loading tiles, take one off id to get index
-}
+
+
+const uint8_t* asset_levels[] = {
+    asset_level0
+};
+
+
+
 
 void render_tiles(std::vector<Tile> tiles) {
     for (int i = 0; i < tiles.size(); i++) {
-        tiles.at(i).render(camera);
+        tiles.at(i).render(levelObjects.camera);
     }
 }
 
 void render_coins(std::vector<Coin> coins) {
     for (int i = 0; i < coins.size(); i++) {
-        coins.at(i).render(camera);
+        coins.at(i).render(levelObjects.camera);
     }
 }
 
 void render_enemies(std::vector<Enemy> enemies) {
     for (int i = 0; i < enemies.size(); i++) {
-        enemies.at(i).render(camera);
+        enemies.at(i).render(levelObjects.camera);
     }
 }
 
@@ -186,7 +304,36 @@ void render_level() {
 void render_entities() {
     render_enemies(levelObjects.enemies);
 
-    levelObjects.player.render(camera);
+    levelObjects.player.render(levelObjects.camera);
+}
+
+void load_level(uint8_t levelNumber) {
+    // Get a pointer to the map header
+    TMX* tmx = (TMX*)asset_levels[levelNumber];
+
+    uint16_t levelWidth = tmx->width;
+    uint16_t levelHeight = tmx->height;
+    uint32_t levelSize = levelWidth * levelHeight;
+
+    levelObjects.foreground.clear();
+    levelObjects.background.clear();
+    levelObjects.coins.clear();
+    levelObjects.enemies.clear();
+
+    for (int i = 0; i < levelSize; i++) {
+        if (tmx->data[i] != EMPTY_TILE_ID) {
+            // Isn't a blank tile, let's add it to the foreground tiles
+            //printf("%u %u %u \n", (i % levelWidth) * SPRITE_SIZE, (i / levelWidth) * SPRITE_SIZE, tmx->data[i]);
+            levelObjects.foreground.push_back(Tile((i % levelWidth) * SPRITE_SIZE, (i / levelWidth) * SPRITE_SIZE, tmx->data[i]));
+        }
+    }
+
+    for (int i = levelSize; i < levelSize * 2; i++) {
+        if (tmx->data[i] != EMPTY_TILE_ID) {
+            // Isn't a blank tile, let's add it to the background tiles
+            levelObjects.background.push_back(Tile((i % levelWidth) * SPRITE_SIZE, (i / levelWidth) * SPRITE_SIZE, tmx->data[i]));
+        }
+    };
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -199,6 +346,8 @@ void init() {
     set_screen_mode(ScreenMode::lores);
 
     screen.sprites = Surface::load(asset_sprites);
+
+    load_level(0); // remove later
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -217,6 +366,9 @@ void render(uint32_t time) {
     screen.alpha = 255;
     screen.mask = nullptr;
     screen.pen = Pen(255, 255, 255);
+
+    render_tiles(levelObjects.background);
+    render_tiles(levelObjects.foreground);
 
     if (gameState == MENU_STATE) {
         screen.text("Press A to Start", minimal_font, Point(SCREEN_WIDTH / 2, 4), true, TextAlign::center_center);
@@ -253,6 +405,4 @@ void update(uint32_t time) {
     else {
         buttonStates.A = 0;
     }
-
-    printf("%d", asset_assets_level01_tmx[0]);
 }
