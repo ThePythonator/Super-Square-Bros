124aedf024a568c40e5498d3f80f28a339cc9527
Phil Howard <phil@gadgetoid.com>
2021-08-18 16:30:58 +0100

    Move unreachable level tiles to Background
    Enhance level collision performance slightly by moving unreachable tiles to the Background layer.
    
    Add Super_Square_Bros_Spritesheet.tsx to make working with the levels easier.

---
+++ assets/Super_Square_Bros_Spritesheet.tsx
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Super_Square_Bros_Spritesheet" tilewidth="8" tileheight="8" tilecount="512" columns="16">
+ <image source="spritesheet.png" width="128" height="256"/>
+ <tile id="384">
+  <animation>
+   <frame tileid="384" duration="150"/>
+   <frame tileid="385" duration="150"/>
+   <frame tileid="386" duration="150"/>
+   <frame tileid="387" duration="150"/>
+   <frame tileid="386" duration="150"/>
+   <frame tileid="385" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="388">
+  <animation>
+   <frame tileid="388" duration="200"/>
+   <frame tileid="389" duration="200"/>
+   <frame tileid="390" duration="200"/>
+   <frame tileid="391" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="432">
+  <animation>
+   <frame tileid="432" duration="150"/>
+   <frame tileid="433" duration="150"/>
+   <frame tileid="434" duration="150"/>
+   <frame tileid="435" duration="150"/>
+   <frame tileid="436" duration="150"/>
+   <frame tileid="437" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
